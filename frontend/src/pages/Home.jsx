dbd9b8b6e81e3b9b16fe75cabb0948913d3e7ef8
lekharsav <your-email@example.com>
2025-12-29 18:41:16 +0530

    rick

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
 
       {/* ✅ WhatsApp Floating Bubble */}
       <a
-        href="https://wa.me/919875680537?text=i%20have%20a%20query."
+        href="https://wa.me/917980071495?text=i%20have%20a%20query."
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 z-50 bg-green-500 hover:bg-green-600 text-white rounded-full p-4 shadow-lg transition-all duration-300 flex items-center justify-center"
